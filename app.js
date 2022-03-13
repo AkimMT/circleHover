454098e4a0f822dadc9f8ac882d14fd2c79ac294
Akbarjon Tulaganov <akimtul@gmail.com>
2022-03-13 22:11:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿
+const circle = document.querySelector('#circle')
+
+circle.addEventListener('mouseenter', () => {
+    if(!circle.classList.contains('hover')) {
+        circle.classList.add('hover');
+    }
+    circle.style.background = 'maroon';
+    circle.textContent = 'Scaled';
+});
+
+circle.addEventListener('mouseleave', () => {
+    if(circle.classList.contains('hover')) {
+        circle.classList.remove('hover');
+    }
+    circle.style.background = 'slateblue';
+    circle.textContent = 'Hover';
+});
+
